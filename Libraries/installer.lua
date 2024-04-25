9c4a7752ef9c237b372046cf8888f8aabbd15e7e
BlankedVoid <94419318+SystemXVoid@users.noreply.github.com>
2024-04-24 23:07:51 -0400

    Update installer.lua

--- Libraries/installer.lua
+++ Libraries/installer.lua
@@ -1,4 +1,5 @@
-game.GetService(game, 'StarterGui'):SetCore('SendNotification', ({
+local startergui = game.GetService(game, 'StarterGui')
+startergui.SetCore(startergui, 'SendNotification', ({
     Title = 'Render', 
     Text = 'The installer is currently disabled right now. Please wait for the announcement of reopening at the discord.', 
     Icon = 'rbxassetid://16498204245',
