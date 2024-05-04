f5fcaf37e607c0bc5fba602d5e9e5cf44737c82a
BlankedVoid <94419318+SystemXVoid@users.noreply.github.com>
2024-05-03 21:15:31 -0400

    Delete Libraries/installer.lua

--- Libraries/installer.lua
+++
@@ -1,6 +0,0 @@
-game.GetService(game, 'StarterGui'):SetCore('SendNotification', ({
-    Title = 'Render', 
-    Text = 'The installer is currently disabled right now. Please wait for the announcement of reopening at the discord.', 
-    Icon = 'rbxassetid://16498204245',
-    Duration = 20
-})) 
