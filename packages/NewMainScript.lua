e2d7498c9d69ce208250a1095411db9b093f8b16
BlankedVoid <94419318+SystemXVoid@users.noreply.github.com>
2024-05-07 20:16:59 -0400

    Update NewMainScript.lua

--- packages/NewMainScript.lua
+++ packages/NewMainScript.lua
@@ -1,6 +1,11 @@
-local httpService = game:GetService('HttpService')
+local cloneref = (cloneref or function(instance) return instance end)
+local lplr = cloneref(game.GetService(game, 'Players')).LocalPlayer
+local httpService = cloneref(game.GetService('HttpService'))
 local identity = (getidentity or syn and syn.getidentity or function() return 2 end)
 local executor = (identifyexecutor or getexecutorname or function() return 'your executor suck ball' end)()
+local hookfunction = (hookfunction or hookfunc or replacefunction or replacefunc or function(func) return func end)
+local hookmetamethod = (hookmetamethod or hookmt or function() end)
+local checkcaller = (checkcaller or function() return false end)
 local setidentity = (setthreadcaps or syn and syn.set_thread_identity or set_thread_identity or setidentity or setthreadidentity or function() end)
 local oldcall
 local isfile = isfile or function(file)
@@ -12,9 +17,17 @@ if shared == nil then
 	getgenv().shared = {} 
 end
 
-if executor == 'Wave' then 
-    loadstring(game.HttpGet(game, 'https://raw.githubusercontent.com/MuhXd/Roblox-mobile-script/main/FileFixes.lua'))() -- credits to MuhXd for making such a wonderful fix for detected executor!!!!
-end
+local oldfunc, oldfunc2
+oldfunc = hookmetamethod(game, '__namecall', function(self, ...)
+	if self == lplr and getnamecallmethod():lower() == 'kick' and not checkcaller() then 
+		return
+	end
+	return oldfunc(self, ...)
+end)
+
+oldfunc2 = hookfunction(lplr.Kick, function(reason)
+	return (checkcaller() and oldfunc2(reason) or nil)
+end)
 
 if isfile('vape/MainScript.lua') then 
 	loadfile('vape/MainScript.lua')()
